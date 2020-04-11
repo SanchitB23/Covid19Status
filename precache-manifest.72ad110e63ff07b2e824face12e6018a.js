1a904f96c773ba7af2cb47816c524ab73acdfcd8
Sanchit Bhatnagar <sanbht23@gmail.com>
2020-04-11 22:18:02 +0530

    Updates

---
+++ precache-manifest.72ad110e63ff07b2e824face12e6018a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42fb5928fd8345740309f07bc7bf462",
+    "url": "/Covid19Status/index.html"
+  },
+  {
+    "revision": "b428434ed37575fc9a5d",
+    "url": "/Covid19Status/static/css/2.b4a16772.chunk.css"
+  },
+  {
+    "revision": "a654e5aeaad1164f927f",
+    "url": "/Covid19Status/static/css/main.894adcee.chunk.css"
+  },
+  {
+    "revision": "b428434ed37575fc9a5d",
+    "url": "/Covid19Status/static/js/2.a429d77c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid19Status/static/js/2.a429d77c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a654e5aeaad1164f927f",
+    "url": "/Covid19Status/static/js/main.66faff12.chunk.js"
+  },
+  {
+    "revision": "13d7517d4f52bc69327c",
+    "url": "/Covid19Status/static/js/runtime-main.74c1be08.js"
+  }
+]);
